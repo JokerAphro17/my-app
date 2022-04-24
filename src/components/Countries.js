4b1be864505b83972a55a11bcd92f9789c9bd27c
Ilboudo Souleymane <ilboudosouleymane4@gmail.com>
2022-04-24 23:01:07 +0000

    frgg

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -4,6 +4,9 @@ import Card from "./Card";
 
 function Countries() {
   const [data, setData] = useState([]);
+  const [range, setRange] = useState(20);
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
+  const [selectedRadio, setSelectedRadio] = useState("");
   // le useEffect permet de faire une requête à l'API pour récupérer les données
   // et les stocker dans le state.
   // Le useEffect est appelé à chaque fois que le state change.
@@ -17,10 +20,42 @@ function Countries() {
   return (
     <div className="countries">
       <h1>COUNTRIES</h1>
+      <ul className="radio-container">
+        <input
+          type="range"
+          min="1"
+          max="250"
+          onChange={(e) => {
+            setRange(e.target.value);
+            console.log(e.target.value);
+          }}
+          defaultValue={range}
+        />
+        {radios.map((continent) => {
+          return (
+            <li>
+              <input
+                type="radio"
+                id={continent}
+                name="radioContinent"
+                onChange={(e) => {
+                  setSelectedRadio(e.target.id);
+                }}
+              />
+              <label htmlFor={continent}>{continent}</label>{" "}
+            </li>
+          );
+        })}
+      </ul>
       <ul>
-        {data.map((pays, index) => (
-          <Card key={index} country={pays} />
-        ))}
+        {data
+          .filter((pays) => {
+            pays.continents[0].includes(selectedRadio);
+          })
+          .slice(0, range)
+          .map((pays, index) => (
+            <Card key={index} country={pays} />
+          ))}
       </ul>
     </div>
   );
